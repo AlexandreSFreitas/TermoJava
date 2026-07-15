992e95fa9fed45717cc31f25762fb0125125da57
Alexandre Freitas Silva <alexandrefreitasx@outlook.com>
2026-07-15 02:00:08 -0300

    Refatora lógica do Termoo em métodos separados

--- src/Comparador.java
+++ src/Comparador.java
@@ -2,10 +2,53 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Comparador {
+    static void main(String[] args) {
+        jogar();
+    }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static  String validarPalavra(Scanner scanner,String palavraSec) {
+        System.out.println("DIGITE A PALAVRA CORRETA: ");
+        String palavraTentada = scanner.next().toUpperCase();
+        while (palavraTentada.length() != palavraSec.length() ){
+            System.out.println("TAMANHO DA PALAVRA ERRADA, APENAS " + palavraSec.length() + " LETRAS");
+            palavraTentada = scanner.next().toUpperCase();
+        }
+        return palavraTentada;
+    }
+    public static String[] comparar(String palavraSec,String palavraTentada) {
+            String[] resultado = new String[palavraSec.length()];
+            boolean[] usado = new boolean[palavraSec.length()];
+
+            for (int i = 0; i < palavraSec.length(); i++) {
+                if (palavraTentada.charAt(i) == palavraSec.charAt(i)) {
+                    usado[i] = true;
+                    resultado[i] = "🟩";
+                }
+            }
+            for (int i = 0; i < palavraSec.length(); i++) {
+                if (!"🟩".equals(resultado[i])) {
+                    for (int j = 0; j < palavraSec.length(); j++) {
+                        if (palavraTentada.charAt(i) == palavraSec.charAt(j) && !usado[j]) {
+                            usado[j] = true;
+                            resultado[i] = "🟨";
+                            break;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < palavraSec.length(); i++) {
+                if (!"🟩".equals(resultado[i]) && !"🟨".equals(resultado[i])) {
+                    resultado[i] = "🟥";
+                }
+            }
+            for (int i = 0; i < resultado.length; i++) {
+                System.out.print(resultado[i]);
+            }
+        return resultado;
+    }
+    public static void  jogar(){
         Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
         String[] palavraSecreta = {
                 "VASCO",
@@ -17,50 +60,22 @@ public class Comparador {
         };
         int indice = random.nextInt(palavraSecreta.length);
         String palavraSec = palavraSecreta[indice];
-
-        for (int tentavias = 0; tentavias < 5; tentavias++){
-            String[] resultado = new String[palavraSec.length()];
-            boolean[] usado = new boolean[palavraSec.length()];
-
-            System.out.println("DIGITE A PALAVRA CORRETA, NUMERO DE TENTATIVAS: " +(tentavias+1) +" de 5");
-            String palavraTentada = scanner.next().toUpperCase();
-            while (palavraTentada.length() != palavraSec.length() ){
-                System.out.println("TAMANHO DA PALAVRA ERRADA, APENAS"+ palavraSec.length() +"LETRAS, NUMERO DE TENTATIVAS: " +(tentavias+1) +" de 5");
-                palavraTentada = scanner.next().toUpperCase();
-            }
-
-            for (int i = 0; i < palavraSec.length(); i++) {
-                if (palavraTentada.charAt(i) == palavraSec.charAt(i)) {
-                    usado[i] = true;
-                    resultado[i] = "🟩";
-                }
-            }
-            for (int i = 0; i < palavraSec.length(); i++) {
-                if (!"🟩".equals(resultado[i])) {
-                    for (int j = 0; j < palavraSec.length(); j++){
-                        if(palavraTentada.charAt(i) == palavraSec.charAt(j) && !usado[j]){
-                            usado[j] = true;
-                            resultado[i] = "🟨";
-                            break;
-                        }
-                    }
-                }
-            }
-            for (int i = 0; i < palavraSec.length(); i++) {
-                if (!"🟩".equals(resultado[i]) && !"🟨".equals(resultado[i])){
-                    resultado[i] = "🟥";
-                }
-            }
-            for (int i = 0; i < resultado.length; i++) {
-                System.out.print(resultado[i]);
-            }
-            if(palavraTentada.equals(palavraSec)){
+        for (int tentativas = 0; tentativas < 5; tentativas++) {
+            String palavraTentada = validarPalavra(scanner,palavraSec);
+            String[] resultado = comparar(palavraSec,palavraTentada);
+            if (palavraTentada.equals(palavraSec)) {
                 System.out.println("Parabéns você ganhou, a palavra era: " + palavraSec);
                 break;
-            }else if(tentavias == 4){
+            } else if (tentativas == 4) {
                 System.out.println("Infelizmente você perdeu, a palavra era: " + palavraSec);
             }
         }
-        }
+
     }
+    }
+
+
+
+
+
 
